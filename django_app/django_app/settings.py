f857b29816ca36440f8d18db880265b2abb6494e
vishnu-k-s <vishnusajeevks@gmail.com>
2022-08-31 10:34:12 +0530

    Change in files for heroku deployment

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5!!ehbf!$t2(tuv96aotzws5s569h&7gl&f4_-n9(%g&rky%d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','carshop-django-postgres.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'carshop-django-postgres.herokuapp.com']
 
 
 # Application definition
